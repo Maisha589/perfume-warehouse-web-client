0d549670494e14ccb810d11a16b098b4ee683bb5
Maisha589 <maisharahman589@gmail.com>
2022-05-07 02:38:52 +0600

    navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Blogs from './Layouts/Blogs/Blogs';
+import Home from './Layouts/Home/Home/Home';
+import Login from './Layouts/Login/Login';
+import PerfumeInventory from './Layouts/PerfumeInventory/PerfumeInventory';
+import Footer from './Layouts/Shared/Footer/Footer';
+import Header from './Layouts/Shared/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      <h2>Royal Perfume</h2>
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/blogs' element={<Blogs></Blogs>}></Route>
+        <Route path='/perfumeInventory' element={<PerfumeInventory></PerfumeInventory>}></Route>
+        <Route path='/login' element={<Login></Login>}>Login</Route>
+      </Routes>
+      <Footer></Footer>
     </div>
   );
 }
