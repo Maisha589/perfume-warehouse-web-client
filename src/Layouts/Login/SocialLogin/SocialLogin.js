cbd622173ab082785709a51fe8255e46608d8139
Maisha589 <maisharahman589@gmail.com>
2022-05-08 02:23:33 +0600

    google authentication added

--- src/Layouts/Login/SocialLogin/SocialLogin.js
+++ src/Layouts/Login/SocialLogin/SocialLogin.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { useNavigate } from 'react-router-dom';
+import auth from '../../../firebase.init';
+import Loading from '../../Shared/Loading/Loading';
 
 const SocialLogin = () => {
+    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    const navigate = useNavigate();
+    let errorElement;
+    if (loading) {
+        return <Loading></Loading>
+    }
+    if (error) {
+        errorElement = <p className='text-danger'>Error: {error?.message}</p>
+    }
+    if (user) {
+        navigate('/');
+    }
+
     return (
         <div>
-
+            {errorElement}
+            < button onClick={() => signInWithGoogle()} className='py-2 px-4 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75' > Google Sign in</button >
         </div>
     );
 };
