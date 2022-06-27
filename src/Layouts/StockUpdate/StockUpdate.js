ab99e5ba859db9b4cc330b852e431677c96b715e
Maisha589 <maisharahman589@gmail.com>
2022-06-27 13:19:10 +0600

    restock

--- src/Layouts/StockUpdate/StockUpdate.js
+++ src/Layouts/StockUpdate/StockUpdate.js
@@ -2,18 +2,28 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 import usePerfume from '../../Hooks/usePerfume';
 import 'react-toastify/dist/ReactToastify.css';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 const StockUpdate = () => {
     const { id } = useParams();
     const [perfume] = usePerfume(id);
+    const [recallStock, setRecallStock] = useState(false);
+    const [quantityData, setQuantityData] = useState({});
+
+    useEffect(() => {
+        const url = `https://floating-lowlands-90113.herokuapp.com/products/${id}`
+        fetch(url)
+            .then(res => res.json())
+            .then(data => setQuantityData(data))
+    }, [id, recallStock])
+
+    // console.log(quantityData);
+    const updateStock = quantityData.stock - 1;
 
     const handleDeliver = () => {
-
-
-        const previousStock = perfume.stock;
-        const updateStock = previousStock - 1;
         const currentStock = { updateStock }
-        console.log(currentStock);
+        // console.log(currentStock);
 
         const url = `https://floating-lowlands-90113.herokuapp.com/products/${id}`;
         fetch(url, {
@@ -25,7 +35,29 @@ const StockUpdate = () => {
         })
             .then(res => res.json())
             .then(result => {
-                console.log("success", result);
+                setRecallStock(!recallStock);
+                // console.log("success", result);
+            })
+    }
+
+    const updateStock2 = quantityData.stock + 1;
+
+    const handleRestock = () => {
+        const currentStock2 = { updateStock2 }
+        // console.log(currentStock2);
+
+        const url = `https://floating-lowlands-90113.herokuapp.com/product/${id}`;
+        fetch(url, {
+            method: "PUT",
+            headers: {
+                "content-type": "application/json"
+            },
+            body: JSON.stringify(currentStock2),
+        })
+            .then(res => res.json())
+            .then(result => {
+                setRecallStock(!recallStock);
+                // console.log("success", result);
             })
     }
 
@@ -43,11 +75,11 @@ const StockUpdate = () => {
                     <br />
                     <p className='font-bold'><span className='font-semibold text-violet-700'>Price:</span>{perfume.price}</p>
                     <br />
-                    <p className='font-bold bold'><span className='font-semibold text-violet-700'>Quantity:</span> <br />{perfume.stock}</p>
+                    <p className='font-bold bold'><span className='font-semibold text-violet-700'>Quantity:</span> <br />{quantityData.stock}</p>
                     <div>
                         <button className='mb-4 mt-3 py-2 px-4 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75' onClick={handleDeliver}>Deliver</button>
                         <br />
-                        <button className=' py-2 px-4 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75' >Restock</button>
+                        <button className=' py-2 px-4 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75' onClick={handleRestock} >Restock</button>
                     </div>
                 </div>
             </div>
